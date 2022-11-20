7abe59785877dddaf1cc93a6aa07a3a87b8c0cc2
dehumanizer <dehumanizer@bigmir.net>
2022-11-20 22:48:28 +0200

    refactored. improved regex validation pattern

---
+++ PS2/Validators/StringValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PS2.Validators
+{
+    public interface IStringValidator
+    {
+        bool Validate(string str);
+    }
+    public class StringValidator: IStringValidator
+    {
+        public bool Validate(string str)
+        {
+            return Regex.IsMatch(str, "^[a-zA-Z0-9!@#$&*{}\\/()_-]*$");
+        }
+    }
+}
